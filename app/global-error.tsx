ac1308be1583ffb758dbf0bfd968da6f5d45b709
Keturah Howard <keturah.dev@gmail.com>
2024-03-05 00:37:00 -0800

    Explore error handling pages

---
+++ app/global-error.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+//this will cover error handling for logic within the app level layout
+const GlobalError = () => {
+  return <div>GlobalError</div>;
+};
+
+export default GlobalError;
