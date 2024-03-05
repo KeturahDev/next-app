cb866bb31ab7289b535d1db6aec4d6cb7e15a162
Keturah Howard <keturah.dev@gmail.com>
2024-03-05 00:15:24 -0800

    experiment with loading UIs

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import UserTable from "./UserTable";
 import Link from "next/link";
 
@@ -13,7 +13,9 @@ const UsersPage = async ({ searchParams: { sortOrder } }: Props) => {
       <Link className="btn" href="/users/new">
         NEW USER
       </Link>
-      <UserTable sortOrder={sortOrder} />
+      <Suspense fallback={<p>Loading...</p>}>
+        <UserTable sortOrder={sortOrder} />
+      </Suspense>
     </div>
   );
 };
