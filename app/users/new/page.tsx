f49a1b83aee06ef1996c7675aee0bf0006daca54
Keturah Howard <keturah.dev@gmail.com>
2024-03-04 23:34:50 -0800

    add programatic navigation & global h1 style

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+"use client";
+import { useRouter } from "next/navigation";
+import React from "react";
 
 //static rendered server page by default
 const NewUserPage = () => {
+  const router = useRouter();
   return (
-    <div>NewUserPage</div>
-  )
-}
+    <button className="btn btn-primary" onClick={() => router.push("/users")}>
+      Create
+    </button>
+  );
+};
 
-export default NewUserPage
+export default NewUserPage;
