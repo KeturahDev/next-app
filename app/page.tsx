61007cc195426f62deb511c1077f43c34693a359
Keturah Howard <keturah.dev@gmail.com>
2024-03-14 19:16:24 -0700

    access session data on server component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import ProductCard from './components/ProductCard'
+import Image from "next/image";
+import Link from "next/link";
+import ProductCard from "./components/ProductCard";
+import { getServerSession } from "next-auth";
+import { AuthOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(AuthOptions);
+
   return (
     <main>
-      <h1>hello wurld</h1>
-      <Link href='/users'>users</Link>
+      <h1>hello {session?.user?.name ? session?.user?.name : "wurld"}</h1>
+      <Link href="/users">users</Link>
       <ProductCard />
     </main>
-  )
+  );
 }
