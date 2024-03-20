b7a670541473eb47e6b75c5aa2ab091139429e62
Keturah Howard <keturah.dev@gmail.com>
2024-03-20 00:32:20 -0700

    style template and add route for sending email

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import {
   Html,
   Body,
   Container,
+  Tailwind,
   Text,
   Link,
   Preview,
@@ -12,14 +13,22 @@ const WelcomeTemplate = ({ name }: { name: string }) => {
   return (
     <Html>
       <Preview> Ahoy There!</Preview>
-      <Body>
-        <Container>
-          <Text> Hello {name}</Text>
-          <Link href="https://github.com/KeturahDev">Here is my github :3</Link>
-        </Container>
-      </Body>
+      <Tailwind>
+        <Body style={body}>
+          <Container>
+            <Text className="font-bold text-3xl"> Hello {name}</Text>
+            <Link href="https://github.com/KeturahDev">
+              Here is my github :3
+            </Link>
+          </Container>
+        </Body>
+      </Tailwind>
     </Html>
   );
+};
+
+const body: CSSProperties = {
+  background: "#E3FFFE",
 };
 
 export default WelcomeTemplate;
