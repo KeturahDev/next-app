5b36727a014485a8cd6fd1f7aae9c17383d352dc
Keturah Howard <keturah.dev@gmail.com>
2024-03-20 17:12:32 -0700

    explore lazy loading in client components

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,7 +1,25 @@
+"use client";
 import React from "react";
+import dynamic from "next/dynamic";
+const HeavyComponent = dynamic(() => import("./HeavyComponent"), {
+  ssr: false, //some cases this prevents serverside errors
+  loading: () => <>Loading...</>,
+});
 
 const TestPage = () => {
-  return <div>TestPage</div>;
+  const [isVisible, setIsVisible] = React.useState(false);
+
+  return (
+    <>
+      <div>TestPage</div>
+
+      <button className="btn" onClick={() => setIsVisible(!isVisible)}>
+        {isVisible ? "Hide" : "Show"}
+      </button>
+
+      {isVisible ? <HeavyComponent /> : null}
+    </>
+  );
 };
 
 export default TestPage;
